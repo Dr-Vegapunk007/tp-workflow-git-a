0ad3841e35df65c6131ce3ff45bb5c459b8fe3a2
Achraf Benchrifa <achraf.benchrifa@gmail.com>
2026-07-09 11:03:10 +0100

    feat: ajout du calcul JS

---
+++ script.js
@@ -0,0 +1,13 @@
+const montantInput = document.getElementById("montant");
+const resultatText = document.getElementById("resultat");
+
+montantInput.addEventListener("input", function () {
+  const montant = parseFloat(montantInput.value);
+
+  if (!isNaN(montant) && montant > 0) {
+    const pourboire = montant * 0.15;
+    resultatText.textContent = ` ${pourboire.toFixed(2)} DH`;
+  } else {
+    resultatText.textContent = "Le pourboire est:";
+  }
+});
